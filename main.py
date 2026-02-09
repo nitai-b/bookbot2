d0fd99ad16526e8a28bcc2cd80371eaae536e67c
nitai-b <nitaibarran5@gmail.com>
2026-02-08 21:21:05 -0400

    new function for geting character count in dictionary

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from stats import get_words_in_book
+from stats import get_character_count
+
 def main():
-    print("greetings boots")
+    print(f"Found {get_words_in_book()} total words")
+    print(get_character_count())
 
 if __name__ == "__main__":
     main()
